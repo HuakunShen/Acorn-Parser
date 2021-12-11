20515e6f97748de63c6d7b2b3c92d681842cc1a4
Huakun Shen <huakun.shen@huakunshen.com>
2021-12-11 12:15:17 -0500

    Init code for vue 3 migration

---
+++ vue3-migration/src/core/lib.ts
@@ -0,0 +1,162 @@
+import {
+  calCoursesWeightSum,
+  number2letterGpaMap,
+  calAvgCoursesWeightedGPA,
+  calAvgCoursesWeightedMark,
+  error,
+  warn,
+} from './utils';
+import { Courses, SessionGpaHdr } from './types';
+
+export class Course {
+  courseCode: string = '';
+  title: string = '';
+  weight: number = 0;
+  mark: number = 0; // integer mark (percentage)
+  numberGrade: number = 0; // GPA in number
+  numberCourseAvg: number = 0; // course average GPA in number
+  opt!: string;
+  courseHdr!: string;
+  complete: boolean = false; // complete when letterGrade !== 'IPR' and have a value
+  constructor(
+    courseCode: string,
+    title: string,
+    weight: number,
+    mark: number,
+    numberGrade: number,
+    numberCourseAvg: number,
+    opt: string,
+    complete: boolean
+  ) {
+    this.courseCode = courseCode;
+    this.title = title;
+    this.weight = weight;
+    this.mark = mark;
+    this.numberGrade = numberGrade;
+    this.numberCourseAvg = numberCourseAvg;
+    this.opt = opt;
+    this.complete = complete;
+  }
+  completed() {
+    return this.complete;
+  }
+}
+
+export class Semester {
+  courses: Courses = [];
+  sessionHdr: string = '';
+  gpaHdr: string = '';
+  year: number;
+  season: string;
+  gpaSummary: SessionGpaHdr; // parsed from GPA header
+
+  constructor(courses: Courses, sessionHdr: string, gpaHdr: string) {
+    this.courses = courses;
+    this.sessionHdr = sessionHdr;
+    this.gpaHdr = gpaHdr;
+    let [year, season] = sessionHdr.split('-')[0].trim().split(' ');
+    this.year = parseInt(year);
+    this.season = season;
+    const gpaStrList = gpaHdr
+      .split('  ')
+      .map((x) => x.trim())
+      .filter((x) => x.length !== 0);
+    this.gpaSummary = {};
+    let i = 0;
+    while (i < gpaStrList.length) {
+      if (gpaStrList[i].includes('GPA') && i !== gpaStrList.length) {
+        this.gpaSummary[gpaStrList[i]] = parseFloat(gpaStrList[i + 1]);
+        i++; // next value should be a GPA and is processed, skip the next
+      } else if (gpaStrList[i].includes('Status:')) {
+        this.gpaSummary.Status = gpaStrList[i].split(':')[1].trim();
+      } else {
+        error(`ERROR: not handled, ${gpaStrList[i]}`);
+      }
+      i++;
+    }
+  }
+
+  /**
+   * @returns total weight of all courses
+   */
+  getTotalWeight(): number {
+    return calCoursesWeightSum(this.courses);
+  }
+
+  /**
+   * @returns total weight of completed courses
+   */
+  getCompletedWeight(): number {
+    return calCoursesWeightSum(this.courses.filter((course) => course.complete));
+  }
+
+  getCompletedCourses(): Courses {
+    return this.courses.filter((course: Course) => course.completed());
+  }
+
+  getAvgNumberGPA(): number {
+    return calAvgCoursesWeightedGPA(this.getCompletedCourses());
+  }
+
+  getAvgLetterGPA(): string {
+    return number2letterGpaMap[this.getAvgNumberGPA()];
+  }
+
+  getAvgMark(): number {
+    return calAvgCoursesWeightedMark(this.getCompletedCourses());
+  }
+}
+
+export class AcademicHistory {
+  semesters: Semester[] = [];
+
+  constructor(semesters: Semester[]) {
+    this.semesters = semesters;
+  }
+
+  /**
+   * @returns flattened semesters, all courses, no matter if it's completed
+   */
+  getAllCourses(): Courses {
+    return this.semesters.map((semester: Semester) => semester.courses).flat();
+  }
+
+  /**
+   * @returns flattened semesters, only completed courses
+   */
+  getCompletedCourses(): Courses {
+    return this.getAllCourses().filter((course: Course) => course.complete);
+  }
+
+  /**
+   * @returns total weight of all courses
+   */
+  getTotalWeight(): number {
+    return calCoursesWeightSum(this.getAllCourses());
+  }
+
+  /**
+   * @returns total weight of completed courses
+   */
+  getCompletedWeight(): number {
+    return calCoursesWeightSum(this.getCompletedCourses());
+  }
+
+  /**
+   * @returns average percentage mark
+   */
+  getAvgMark(): number {
+    return calAvgCoursesWeightedMark(this.getCompletedCourses());
+  }
+
+  /**
+   * @returns average GPA in number
+   */
+  getNumberCGPA(): number {
+    return calAvgCoursesWeightedGPA(this.getCompletedCourses());
+  }
+
+  getLetterCGPA(): string {
+    return number2letterGpaMap[this.getNumberCGPA()];
+  }
+}
---
+++ vue3-migration/src/popup/views/About.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <el-button>button</el-button>
+  </div>
+</template>
+
+<script>
+import { ElButton } from 'element-plus';
+export default {
+  components: { ElButton },
+};
+</script>
