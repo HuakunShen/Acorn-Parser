017128079c0679ed9985a6bb07271f1db8b23b1c
Huakun Shen <huakun.shen@huakunshen.com>
2022-05-15 21:00:21 -0400

    A new version vue 3 starter code uploaded to replace old code
    Element UI, vue3 starter code and other stuffs all have updates. I will rewrite everything.

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,12 @@
-import { log, getColumnHeaderInfo, sessionTableStr2Obj } from "./core/utils";
-import {
-  sampleTables,
-  sampleGpaStr,
-  sampleSessionStr,
-  sampleHeaderStr,
-} from "./core/sample_data";
-import { AcademicHistory, Semester, Course } from "./core/lib";
-const colHeaderInfo = getColumnHeaderInfo(sampleHeaderStr);
-const sessionTables = sampleTables.map((tableStr) =>
-  sessionTableStr2Obj(tableStr, sampleSessionStr, sampleGpaStr, colHeaderInfo)
-);
-// log(sessionTables[0]);
-const ah = new AcademicHistory(sessionTables);
-const jsonStr = JSON.stringify(ah, null, 4);
-// log(ah);
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 
-// save to fs
-// fs.writeFileSync(path.resolve("history.json"), jsonStr);
+import App from './App.vue'
+import router from './router'
 
-const rawdata = jsonStr;
-// load from fs
-// let rawdata = fs.readFileSync('history.json');
+const app = createApp(App)
 
-const newAH = AcademicHistory.loadFromJson(rawdata);
-log(newAH);
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
