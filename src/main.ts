cffe8e16b33d95ca5e1f8a0cc6231de319a407e5
Huakun Shen <huakun.shen@inossem.com>
2022-05-15 23:34:16 -0400

    Enabled element plus auto import instead of full import

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,11 @@ import App from './App.vue';
 import router from './router';
 import './index.css';
 
-import ElementPlus from 'element-plus';
+// import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
 
 const app = createApp(App);
 
-app.use(createPinia()).use(router).use(ElementPlus);
+app.use(createPinia()).use(router)
 
 app.mount('#app');
