8de57a674bcc792acb99f52e3fd2512a7f02a328
Huakun Shen <huakun.shen@huakunshen.com>
2022-05-17 03:35:47 -0400

    Multi-page extension Working

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
-import { fileURLToPath, URL } from 'url';
+import { fileURLToPath, URL } from "url";
 
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import vueJsx from '@vitejs/plugin-vue-jsx';
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import { resolve } from "path";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 
-import AutoImport from 'unplugin-auto-import/vite';
-import Components from 'unplugin-vue-components/vite';
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
+const root = resolve(__dirname, "src");
+const outDir = resolve(__dirname, "dist");
+const publicDir = resolve(__dirname, "public");
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -22,10 +26,19 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  publicDir,
+  root,
   build: {
-    outDir: 'dist/ui',
+    outDir,
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        popup: resolve(root, "index.html"),
+        options: resolve(root, "pages/options/index.html"),
+      },
+    },
   },
 });
